3def1ac523b7de6b1f38a446771c94bc8cd16e03
Jules-christ GBASSI <juleschristgbassi9@gmail.com>
2025-12-03 15:42:51 +0100

    some update

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,14 @@
 use App\Http\Controllers\api\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('check.api.key')->group(function () {
+//Afficher tous les posts disponibles
+Route::get('posts', [PostController::class, 'index']);
 
-    Route::get('posts', [PostController::class, 'index']);
-    Route::post('posts/create', [PostController::class, 'store']);
-    Route::put('posts/edit/{id}', [PostController::class, 'update']);
-    Route::delete('post/{id}', [PostController::class, 'delete']);
+//Créer un nouvel post
+Route::post('posts/create', [PostController::class, 'store']);
 
-});
+//Mettre à jour un post
+Route::put('posts/edit/{id}', [PostController::class, 'update']);
+
+//Supprimer un post
+Route::delete('post/{id}', [PostController::class, 'delete']);
